4a37701c5c4d2a04f49a9d974fa0eb1c50393409
cagrigsby <162646448+cagrigsby@users.noreply.github.com>
2024-10-02 00:24:28 -0500

    Update vbenum.sh

--- vbenum.sh
+++ vbenum.sh
@@ -8,5 +8,13 @@ run_command() {
 
 # Enumeration tasks with titles
 run_command "HOSTNAME:" "hostname"
-run_command "WRITABLE DIRECTORIES:" "find / -writable -type d 2>/dev/null"
+run_command "SYSTEM INFO:" "cat /etc/issue; cat /etc/*-release; uname -r; arch"
+run_command "ANYTHING IN /OPT:" "ls /opt"
+run_command "ANYTHING IN /VAR/MAIL:" "ls /var/mail"
+run_command "ANYTHING IN /HOME:" "ls home"
 run_command "USERS:" "cat /etc/passwd | cut -d : -f 1"
+run_command "SUID BINARIES:" "find / -perm -u=s -type f 2>/dev/null"
+run_command "ODD FILES:" "find / -type f -name -o -name "*.txt" -o -name "*.kdbx" -o -name "*.zip" 2>/dev/null"
+run_command "WRITABLE DIRECTORIES:" "find / -writable -type d 2>/dev/null"
+
+echo "Now run 'sudo -l', 'pspy64', and 'linpeas.sh'.

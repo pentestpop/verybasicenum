c43bd1d9891f13d8a07a67c0e18a28f4f08b08ab
pentestpop <162646448+pentestpop@users.noreply.github.com>
2024-10-07 15:39:16 -0500

    Update vbenum.bat

--- vbenum.bat
+++ vbenum.bat
@@ -1,92 +1,79 @@
 @echo off
+color 0E
 echo ===========================
+color 0C
 echo Whoami:
+color 0E
 echo ===========================
+color 0F
 whoami
 echo.
 
+color 0E
 echo ===========================
+color 0C
 echo Privileges:
+color 0E
 echo ===========================
+color 0F
 whoami /priv
 echo.
 
+color 0E
 echo ===========================
+color 0C
 echo User Information:
+color 0E
 echo ===========================
+color 0F
 net user
 echo.
 
+color 0E
 echo ===========================
+color 0C
 echo Domain User Information:
+color 0E
 echo ===========================
+color 0F
 net user /domain
 echo.
 
+color 0E
 echo ===========================
+color 0C
 echo System Information:
+color 0E
 echo ===========================
+color 0F
 systeminfo
 echo.
 
+color 0E
 echo ===========================
+color 0C
 echo Network Configuration:
+color 0E
 echo ===========================
+color 0F
 ipconfig
 echo.
 
+color 0E
 echo ===========================
+color 0C
 echo C Drive:
+color 0E
 echo ===========================
+color 0F
 dir C:\
 echo.
 
+color 0E
 echo ===========================
+color 0C
 echo Installed 32 bit:
+color 0E
 echo ===========================
-powershell -Command "Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | select displayname"
-echo.
-
-echo ===========================
-echo Installed 64 bit:
-echo ===========================
-powershell -Command "Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object displayname"
-echo.
-
-echo ===========================
-echo Running Processes:
-echo ===========================
-powershell -Command "Get-Process"
-echo.
-
-echo ===========================
-echo History:
-echo ===========================
-powershell -Command "Get-Content '$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt'"
-echo.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+color 0F
+powershell -Command "Get

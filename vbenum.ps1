f9b9c5ad9dab0723af9490acb136cdf3e9ff255a
pentestpop <162646448+pentestpop@users.noreply.github.com>
2024-10-07 15:37:07 -0500

    Update vbenum.ps1

--- vbenum.ps1
+++ vbenum.ps1
@@ -2,78 +2,79 @@
 $ProgressPreference = 'SilentlyContinue'
 
 # Whoami equivalent
-Write-Host "=========================="
-Write-Host "Whoami:"
-Write-Host "=========================="
-whoami
+
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Whoami:" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+whoami | Write-Host -ForegroundColor White
 Write-Host ""
 
 # Privileges (using Whoami /priv)
-Write-Host "=========================="
-Write-Host "Privileges:"
-Write-Host "=========================="
-whoami /priv
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Privileges:" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+whoami /priv | Write-Host -ForegroundColor White
 Write-Host ""
 
 # User Information
-Write-Host "=========================="
-Write-Host "User Information:"
-Write-Host "=========================="
-net user
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "User Information:" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+net user | Write-Host -ForegroundColor White
 Write-Host ""
 
 # Domain User Information
-Write-Host "=========================="
-Write-Host "Domain User Information:"
-Write-Host "=========================="
-net user /domain
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Domain User Information:" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+net user /domain | Write-Host -ForegroundColor White
 Write-Host ""
 
 # System Information
-Write-Host "=========================="
-Write-Host "System Information:"
-Write-Host "=========================="
-systeminfo
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "System Information:" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+systeminfo | Write-Host -ForegroundColor White
 Write-Host ""
 
 # Network Configuration (IPConfig)
-Write-Host "=========================="
-Write-Host "Network Configuration:"
-Write-Host "=========================="
-ipconfig
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Network Configuration:" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+ipconfig | Write-Host -ForegroundColor White
 Write-Host ""
 
 # Directory Listing
-Write-Host "=========================="
-Write-Host "Directory Listing of C:\"
-Write-Host "=========================="
-Get-ChildItem C:\
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Directory Listing of C:\" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+Get-ChildItem C:\ | Write-Host -ForegroundColor White
 Write-Host ""
 
 # Installed Programs from Wow6432Node (32-bit applications)
-Write-Host "=========================="
-Write-Host "Installed Programs (Wow6432Node):"
-Write-Host "=========================="
-Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Installed Programs (32 Bit):" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName | Write-Host -ForegroundColor White
 Write-Host ""
 
 # Installed Programs from HKLM (64-bit applications)
-Write-Host "=========================="
-Write-Host "Installed Programs (HKLM):"
-Write-Host "=========================="
-Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Installed Programs (64 Bit):" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName | Write-Host -ForegroundColor White
 Write-Host ""
 
 # Running Processes
-Write-Host "=========================="
-Write-Host "Running Processes:"
-Write-Host "=========================="
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "Running Processes:" -ForegroundColor Red | Write-Host -ForegroundColor White
+Write-Host "==========================" -ForegroundColor Yellow
 Get-Process
 Write-Host ""
 
 # PowerShell History
-Write-Host "=========================="
-Write-Host "PowerShell ConsoleHost History:"
-Write-Host "=========================="
-Get-Content "$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+Write-Host "==========================" -ForegroundColor Yellow
+Write-Host "PowerShell ConsoleHost History:" -ForegroundColor Red
+Write-Host "==========================" -ForegroundColor Yellow
+Get-Content "$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" | Write-Host -ForegroundColor White
 Write-Host ""
